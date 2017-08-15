c488d203d2765897da2d3ac244122cadeb3cabb9
Michael Chapman <submariner100@gmail.com>
2017-08-15 21:10:58 +0100

    first commit

---
+++ Project37/CardViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CardViewController.swift
+//  Project37
+//
+//  Created by Macbook on 15/08/2017.
+//  Copyright © 2017 Chappy-App. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+	
+	weak var delegate: ViewController!
+	
+	var front: UIImageView!
+	var back: UIImageView!
+	var isCorrect = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+	
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project37/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Project37
+//
+//  Created by Macbook on 15/08/2017.
+//  Copyright © 2017 Chappy-App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBOutlet weak var cardContainer: UIView!
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
