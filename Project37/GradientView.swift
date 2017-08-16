5e064a0bb5c4e1162b856f81266ba5e69fe6dd82
Michael Chapman <submariner100@gmail.com>
2017-08-16 12:05:49 +0100

    second commit

---
+++ Project37/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Project37
+//
+//  Created by Macbook on 16/08/2017.
+//  Copyright © 2017 Chappy-App. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
